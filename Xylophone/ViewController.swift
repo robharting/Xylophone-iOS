c0a220c377b9c13f8442cf8e2dfe4daa4906b0c6
Rob Harting <robharting@hotmail.com>
2024-04-19 15:52:27 +0200

    made button c play sound

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+    
+        print("Red C Pressed")
+        playSound()
+        
+    }
     
 
+    @IBAction func keyDPressed(_ sender: UIButton) {
+        
+        print("Orange D Pressed")
+    }
+    
+    func playSound() {
+            let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+            player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                    
+        }
 }
 
